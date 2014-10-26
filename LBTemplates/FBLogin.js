4bf9039847efa6489b471516ee104060d32d09de
gilsefti <gilsefti@gmail.com>
2014-10-26 10:26:40 +0200

    foodP

--- LBTemplates/FBLogin.js
+++ LBTemplates/FBLogin.js
@@ -1,29 +1,30 @@
 mainApp.controller('FBLoginCtrl', function ($scope, UserService, $state) {
 
     $scope.fbLogin = function () {
-        $state.go("Search"); 
+        $state.go("Search");
         if (!window.cordova) {
             var appId = "509210995889450";//prompt("Enter FB Application ID", "");
             facebookConnectPlugin.browserInit(appId);
         }
-        var success = function () {           
-                $state.go("LB");  
+        var success = function () {
+            alert(JSON.stringify(response));
+            $state.go("LB");
         }
         var fail = function (response) {
             alert(JSON.stringify(response));
         }
 
         facebookConnectPlugin.login([], success, fail);
-   
+
     }
 
-   
+
 
     $scope.logout = function () {
         facebookConnectPlugin.logout(success, fail);
         var success = function (response) {
-        
-         
+
+
         }
         var fail = function (response) { alert("logout failed") }
     }
